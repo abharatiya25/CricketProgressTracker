5c77b22fc7f33e7b99009beb908bba3df340d038
Ankit Bharatiya <indikool21@yahoo.com>
2023-09-21 11:42:38 -0400

    Initial Commit

---
+++ CricketProgressTracker/CricketProgressTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CricketProgressTrackerApp.swift
+//  CricketProgressTracker
+//
+//  Created by Ankit Bharatiya on 9/21/23.
+//
+
+import SwiftUI
+
+@main
+struct CricketProgressTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
